f50d68d992f6a6366ac17fb43e424c17d51fd820
WGXWGX <wgx15145076862@sina.com>
2016-11-13 09:05:15 +0800

    Update module.js

--- module.js
+++ module.js
@@ -15,3 +15,29 @@ exports  与  module.exports的区别
 3.require()返回的是（拿到的）module.exports  而不是exports
 4.导出非对象接口就必须是module.exports
 5.lizi     如上
+
+1----------------
+function person(){
+	this.eat = function(){
+		console.log('the food is good');
+	}
+	this.say = function(){
+		console.log('laoshan ');
+	}
+}
+module.exports = person;
+2-------------------
+exports.person={
+	'eat':function(){
+		console.log('eat');
+	},
+	'say':function(){
+		console.log('say');
+	}
+}
+3-------------------
+module.exports = {'arr' : ['wendy','john','susan']};
+exports.arr = ['a','b','c'];    arr就是那个对象
+两行代码的意思是一样的，运行时删掉注释
+
+

c59ff14b3f08ce46064fcbe44282ce9ae8ef4379
WGXWGX <wgx15145076862@sina.com>
2016-11-13 09:03:48 +0800

    Update module.js
    完善

--- module.js
+++ module.js
@@ -10,3 +10,8 @@ exports.sayHello=function(){
 
 
 exports  与  module.exports的区别
+1.exports 是 module.exports的引用
+2.module.exports的初始值是一个空对象，exports的初始值也是空对象.
+3.require()返回的是（拿到的）module.exports  而不是exports
+4.导出非对象接口就必须是module.exports
+5.lizi     如上

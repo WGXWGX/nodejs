3baf5cb04910848aebfadba8f0ee5f8b173a223c
WGXWGX <wgx15145076862@sina.com>
2016-11-14 12:42:36 +0800

    Update call.js

--- call.js
+++ call.js
@@ -11,3 +11,17 @@ console.log(obj.showText());  //laoshan    undefined
 obj.showText();
 //两种输出方式都行
 
+
+function class1(){
+	this.name = 'laoshan';
+	this.showText = function(){
+		console.log(this.name);
+	}
+}
+class1.prototype.aa = function() {//原型链的方法定义函数
+	console.log('aa');
+};
+var class1 = new class1();
+class1.showText();
+class1.aa();
+
